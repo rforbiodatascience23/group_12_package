72dbaa5200e82cf8abb48f3e18acdc4b18030a57
Arlinda96 <s223712@dtu.dk>
2023-10-24 23:01:49 +0200

    function 5 update

--- R/col_codon.R
+++ R/col_codon.R
@@ -1,8 +1,8 @@
 #' Plotting amino acid counts
 #'
-#' @param codon_table is a dataset of codons and aminoacid sympols
+#' @param codon_table A dataset of codons and amino acid symbols
 #'
-#' @return a column plot of aminoacid counts???
+#' @return A column plot of the unique amino acid symbols
 #' @export
 #'
 # @examples
@@ -10,7 +10,7 @@
 # This is how I changed the name_mes - I think that the name_me2 and the Name_me2 are different, but I dont know what a good name for Name_me2 should be..???
 # name_me1 = amino_acid_plot
 # name_me2 = codon_table
-# Name_me2 = Name_me2 (I dont know what this is supposed to be????)
+# Name_me2 = Unique_amino_acids
 # name_me3 = unique_amino_acid_symbols
 # name_me4 = graph_col
 
@@ -29,11 +29,11 @@ amino_acid_plot <- function(codon_table){
   #this code counts the amino acid symbols in the codon table, and returns it as a dataframe.
 
   colnames(counts) <- c("Counts")
-  counts[["Name_me2"]] <-  rownames(counts)
+  counts[["Unique_amino_acids"]] <-  rownames(counts)
   #there is something wrong with this code - I dont know what it does, but it gives me NAs as column names and the Name_me2 column is just the row numbers????
 
   my_col_plot <- counts |>
-    ggplot2::ggplot(ggplot2::aes(x = Name_me2, y = Counts, fill = Name_me2)) +
+    ggplot2::ggplot(ggplot2::aes(x = Unique_amino_acids, y = Counts, fill = Unique_amino_acids)) +
     ggplot2::geom_col() +
     ggplot2::theme_bw() +
     ggplot2::theme(legend.position = "none")

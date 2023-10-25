ab1157fec41bc874f5210a45a95968878d76a005
DardeDTU <camilla.h.drabe@gmail.com>
2023-10-25 17:00:02 +0200

    small changes in function five + added function five to the vingette

--- R/col_codon.R
+++ R/col_codon.R
@@ -1,13 +1,13 @@
 #' Plotting amino acid counts
 #'
-#' @param codon_table A dataset of codons and amino acid symbols
+#' @param seq A data set of codons and amino acid symbols
 #'
-#' @return A column plot of the unique amino acid symbols
+#' @return A column plot of the count of unique amino acid symbols
 #' @export
 #'
 # @examples
 
-# This is how I changed the name_mes - I think that the name_me2 and the Name_me2 are different, but I dont know what a good name for Name_me2 should be..???
+# This is how we changed the name_mes - NB name_me2 and the Name_me2 are different.
 # name_me1 = amino_acid_plot
 # name_me2 = seq
 # Name_me2 = Unique_amino_acids
@@ -30,14 +30,14 @@ amino_acid_plot <- function(seq){
 
   colnames(counts) <- c("Counts")
   counts[["Unique_amino_acids"]] <-  rownames(counts)
-  #there is something wrong with this code - I dont know what it does, but it gives me NAs as column names and the Name_me2 column is just the row numbers????
+  #there is something wrong with this code -  it gives NAs as column names and the Unique_amino_acids column is just the row numbers????
 
   my_col_plot <- counts |>
     ggplot2::ggplot(ggplot2::aes(x = Unique_amino_acids, y = Counts, fill = Unique_amino_acids)) +
     ggplot2::geom_col() +
     ggplot2::theme_bw() +
     ggplot2::theme(legend.position = "none")
-  #this code creates a column plot, however it does not seem to be the correct x and y, probably due to some error in the colnames/rownames code section
+  #this code creates a column plot, however it does not seem to be the correct x and y, probably due to the warning-error in the colnames/rownames code section
   return(my_col_plot)
   #this code returns the plot
 }
